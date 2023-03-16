97c071726d86c0e572782b5e9c3f2d5d6234895f
Boris Kolesnikov <barbariskolesnikov@gmail.com>
2023-03-16 09:44:09 +0300

    add echo response

--- functions.cpp
+++ functions.cpp
@@ -18,14 +18,20 @@ QByteArray func_login(QString username, QString password) {
 }
 
 QByteArray func_register(QString username, QString email, QString password) {
-    return QByteArray("Тут должна быть регистрация\n");
+    return QByteArray("Тут должна быть регистрация\r\n");
 }
 
 QByteArray parse(QString user_message) {
     QStringList data = user_message.split(' ');
-    if (data[0] == "register" && data.size() == 4)
-        return func_register(data[1], data[2], data[3]);
-    else if (data[0] == "login" && data.size() == 3)
-        return func_login(data[1], data[2]);
-    return wrong_user_message(data);
+    if (data[0] == "register")
+        if (data.size() == 4)
+            return func_register(data[1], data[2], data[3]);
+        else
+            return wrong_user_message(data);
+    else if (data[0] == "login")
+        if ( data.size() == 3)
+            return func_login(data[1], data[2]);
+        else
+            return wrong_user_message(data);
+    return QByteArray(user_message.toUtf8());
 }
