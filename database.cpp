14c7c7a048b367d73f7dcb6c28cfb99cc20e2575
Boris Kolesnikov <barbariskolesnikov@gmail.com>
2023-04-05 17:32:55 +0300

    Add comments to database.cpp

--- database.cpp
+++ database.cpp
@@ -51,42 +51,54 @@ void DataBase::create_db()
 
 DataBase::DataBase()
 {
+    // Инициализируем базу данных из файла database.sqlite
     this->db = QSqlDatabase::addDatabase("QSQLITE");
     this->db.setDatabaseName("database.sqlite");
+
+    // Выводим ошибки, если они есть, в консоль
     if (!(this->db.open()))
         qDebug()<<db.lastError().text();
+
+    // Если нет таблиц Users, Tasks, то создаем их
     if (!this->db_created())
         this->create_db();
 }
 
 DataBase::~DataBase()
 {
+    // Закрываем базу данных
     db.close();
 }
 
 QVector<QMap<QString, QString>> DataBase::db_request(QString request)
 {
     QSqlQuery query(this->db);
-    query.exec(request);
+    query.exec(request);            // исполняем запрос
+
+    QVector<QMap<QString, QString>> result; // таблица с запрашиваемыми данными
+
+    if (query.lastError().isValid()) {
+        qDebug() << "Ошибка выполнения запроса:" << query.lastError().text();
+        return result;
+    }
+
     QSqlRecord record = query.record();
-    QVector<QString> columns;
+    QVector<QString> columns;       // хранит столбцы возвращаемой таблицы
+
     for (int i = 0; i < record.count(); i++) {
         QString columnName = record.fieldName(i);
         columns.append(columnName);
     }
-    QVector<QMap<QString, QString>> result;
-    if (query.lastError().isValid()) {
-        qDebug() << "Ошибка выполнения запроса:" << query.lastError().text();
-    } else {
-        while (query.next()) {
-            QMap<QString, QString> row;
-            for (int i = 0; i < columns.count(); i++) {
-                QString columnName = query.record().fieldName(i);
-                QString value = query.value(i).toString();
-                row.insert(columnName, value);
-            }
-            result.append(row);
+
+    while (query.next()) {
+        QMap<QString, QString> row;
+        for (int i = 0; i < columns.count(); i++) {
+            // строки таблицы заполняются, каждая строка - хэшмап, ключ - название столбца, значение - то, что в ячейке
+            QString columnName = query.record().fieldName(i);
+            QString value = query.value(i).toString();
+            row.insert(columnName, value);
         }
+        result.append(row);
     }
     return result;
 }
@@ -123,6 +135,7 @@ void DataBase::printTable(const QList<QMap<QString, QString>>& table)
 
 void DataBase::db_clear()
 {
+    // очищаются таблицы Users и Tasks, но не удаляются
     QSqlQuery query(this->db);
     query.exec("DELETE FROM TABLE Users");
     query.exec("DELETE FROM TABLE Tasks");
--- main.cpp
+++ main.cpp
@@ -13,10 +13,9 @@ int main(int argc, char *argv[])
 
     MyTcpServer *b = MyTcpServer::getInstance();
 
+    // Значения для проверки работоспособности
     DBRequests::getInstance()->reg("vasya", "123", 0);
-
     DBRequests::getInstance()->auth("vasya", "123", 123);
-
     DBRequests::getInstance()->task_is_done(123,1,123,true);
 
     return a.exec();
