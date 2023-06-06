bdea30c50477f36b8a1bd20995d93e90f712c670
Boris Kolesnikov <barbariskolesnikov@gmail.com>
2023-06-06 16:28:32 +0300

    Исправлены unittest-ы для 1 задания

--- server_testing/tst_check_task1_test.cpp
+++ server_testing/tst_check_task1_test.cpp
@@ -28,12 +28,15 @@ check_task1_test::~check_task1_test()
 
 void check_task1_test::test_case1()
 {
-    QVERIFY2(typeid(TaskManager::getInstance()->create_task(1)).name() == QString("7QString"), typeid(TaskManager::getInstance()->create_task(1)).name());
+    QVERIFY2(typeid(TaskManager::getInstance()->create_task(1)).name() == QString("7QString"),
+            QString("Неверный тип возвращаемого значения: %1. Ожидается - 7QString")
+             .arg(typeid(TaskManager::getInstance()->create_task(1)).name()).toUtf8());
 }
 
 void check_task1_test::test_case2()
 {
-    QVERIFY2(TaskManager::getInstance()->check_task(1, 1, "12 23") == true, "Неверная проверка задачи 1");
+    QVERIFY2(TaskManager::getInstance()->check_task(1, 1, "12 23") == true,
+             "Неверная проверка задачи 1: false. Ожидается - true");
 }
 
 QTEST_APPLESS_MAIN(check_task1_test)
